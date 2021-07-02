b03f3cd8001a5504d4add369acde9907c4c77973
xueshuming <xueshuming@minigui.org>
2021-07-02 10:18:50 +0800

    test_rwstream: 1. remove unused code; 2. add create/remove temp file function

--- test/rwstream/test_rwstream.cpp
+++ test/rwstream/test_rwstream.cpp
@@ -1,19 +1,35 @@
-#include <gtest/gtest.h>
 #include "rwstream.h"
 
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+#include <stdio.h>
+#include <gtest/gtest.h>
 
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+
+void create_temp_file(const char* file, const char* buf, size_t buf_len)
+{
+    FILE* fp = fopen(file, "wb");
+    fwrite(buf, buf_len, 1, fp);
+    fflush(fp);
+    fclose(fp);
 }
 
+void remove_temp_file(const char* file)
+{
+    remove(file);
+}
 
 TEST(stdio_rwstream, new_destroy) {
-  purc_rwstream_t rws = purc_rwstream_new_from_file("/tmp/x.c", "r");
-  ASSERT_NE(rws, nullptr);
+    char tmp_file[] = "/tmp/stdio.txt";
+    char buf[] = "This is test file. 这是测试文件。";
+    create_temp_file(tmp_file, buf, strlen(buf));
 
-  int ret = purc_rwstream_close(rws);
-  ASSERT_EQ(ret, 0);
+    purc_rwstream_t rws = purc_rwstream_new_from_file(tmp_file, "r");
+    ASSERT_NE(rws, nullptr);
+
+    int ret = purc_rwstream_close(rws);
+    ASSERT_EQ(ret, 0);
+
+    ret = purc_rwstream_destroy (rws);
+    ASSERT_EQ(ret, 0);
+
+    remove_temp_file(tmp_file);
 }
