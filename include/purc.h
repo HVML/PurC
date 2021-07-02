be1f7df5359239ccb31bf4f877a028f646df1502
Vincent Wei <vincent@minigui.org>
2021-07-02 18:30:36 +0800

    remove old headers

--- include/purc.h
+++ include/purc.h
@@ -1,48 +1,38 @@
-/*
-** Copyright (C) 2015-2017 Alexander Borisov
-**
-** This file is a part of PurC (short for Purring Cat), an HVML parser
-** and interpreter.
-** 
-** This program is free software: you can redistribute it and/or modify
-** it under the terms of the GNU Lesser General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU Lesser General Public License for more details.
-**
-** You should have received a copy of the GNU Lesser General Public License
-** along with this program.  If not, see <https://www.gnu.org/licenses/>.
-**
-** Author: lex.borisov@gmail.com (Alexander Borisov)
-*/
+/**
+ * @file purc.h
+ * @author Vincent Wei (https://github.com/VincentWei)
+ * @date 2021/07/02
+ * @brief The main header file of PurC.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML parser
+ * and interpreter.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef PURC_PURC_H
 #define PURC_PURC_H
-
-#pragma once
-
-/**
- * @file purc.h
- *
- * Fast C/C++ HVML Parser and Interperter. Using threads.
- * With possibility of a Single Mode.
- * 
- * C99 and POSIX Threads!
- */
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 
-#include "purc_version.h"
 #include "purc_macros.h"
-#include "mycore.h"
-#include "myencoding.h"
-#include "myhtml.h"
-#include "myhvml.h"
+#include "purc_version.h"
+#include "purc_errors.h"
 
 #endif /* PURC_PURC_H */
