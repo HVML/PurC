96cf54da219d2388248f2eef59d463f68efb92bb
gengyue <gengyue@minigui.org>
2021-07-06 18:53:40 +0800

    delete variant_config.h

--- Source/PurC/include/private/variant.h
+++ Source/PurC/include/private/variant.h
@@ -1,8 +1,8 @@
-/*
- * @file variant.h
- * @author Vincent Wei (https://github.com/VincentWei)
- * @date 2021/07/05
- * @brief The structures for PurC variant
+/**
+ * @file variant-type.h
+ * @author 
+ * @date 2021/07/02
+ * @brief The API for variant.
  *
  * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
  *
@@ -22,40 +22,43 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef PURC_PRIVATE_VARIANT_H
-#define PURC_PRIVATE_VARIANT_H
 
-#include "purc.h"
 
-#include "config.h"
+#ifndef _VARIANT_TYPES_H_
+#define _VARIANT_TYPES_H_
 
-#define PCVARIANT_TYPE_UNDEFINED        0
-#define PCVARIANT_TYPE_NULL             1
-#define PCVARIANT_TYPE_BOOLEAN          2
-#define PCVARIANT_TYPE_NUMBER           3
-#define PCVARIANT_TYPE_LONGINT          4
-#define PCVARIANT_TYPE_LONGUINT         5
-#define PCVARIANT_TYPE_LONGDOUBLE       6
-#define PCVARIANT_TYPE_STRING           7
-#define PCVARIANT_TYPE_BYTESEQ          8
-#define PCVARIANT_TYPE_DYNAMIC          9
-#define PCVARIANT_TYPE_NATIVE          10
+#include "purc_variant.h"
 
-#define PCVARIANT_TYPE_OBJECT          11
-#define PCVARIANT_TYPE_ARRAY           12
-#define PCVARIANT_TYPE_SET             13
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
 
+// define errors for variant
+#define PURC_ERROR_VARIANT_NO_MEMORY        (PURC_ERROR_FIRST_VARIANT + 0)
+#define PURC_ERROR_VARIANT_INVALID_VALUE    (PURC_ERROR_FIRST_VARIANT + 1)
+#define PURC_ERROR_VARIANT_INVALID_TYPE     (PURC_ERROR_FIRST_VARIANT + 2)
+
+#define MAX(a, b)   (a) > (b)? (a): (b);
 
 #define PCVARIANT_FLAG_NOREF     0x0001
 #define PCVARIANT_FLAG_NOFREE    0x0002
-#define PCVARIANT_FLAG_LONG      0x0004
+
+// for variant const, registered in thread instance
+struct purc_variant_const {
+    struct purc_variant_t pcvariant_null;
+    struct purc_variant_t pcvariant_undefined;
+    struct purc_variant_t pcvariant_false;
+    struct purc_variant_t pcvariant_true;
+}
+
 
 struct purc_variant {
+
     /* variant type */
-    unsigned int type:8;
+    enum variant_type type:8;
 
     /* real length for short string and byte sequence */
-    unsigned int size:8;  // not-counting-null-terminator if string
+    unsigned int size:8;        
 
     /* flags */
     unsigned int flags:16;
@@ -84,13 +87,16 @@ struct purc_variant {
         void*       ptr2[2];
 
         /* for long string, long byte sequence, array, and object (one for size and the other for pointer). */
-        uintptr_t   sz_ptr[2];  // not-counting-null-terminator if long string
+        uintptr_t   sz_ptr[2];
 
         /* for short string and byte sequence; the real space size of `bytes` is `max(sizeof(long double), sizeof(void*) * 2)` */
         uint8_t     bytes[0];
-    } u;
+    };
 };
 
-#endif /* not defined PURC_PRIVATE_VARIANT_H */
 
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
 
+#endif  /* _VARIANT_TYPES_H_ */
