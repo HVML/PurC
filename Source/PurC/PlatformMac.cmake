23e83fc2799d0d3bb94e2298a81956f03c385447
Vincent Wei <vincent@minigui.org>
2021-07-06 16:53:28 +0800

    initial version for Mac platform

---
+++ Source/PurC/PlatformMac.cmake
@@ -0,0 +1,25 @@
+set(PurC_OUTPUT_NAME purc)
+
+list(APPEND PurC_PRIVATE_INCLUDE_DIRECTORIES
+)
+
+list(APPEND PurC_UNIFIED_SOURCE_LIST_FILES
+)
+
+list(APPEND PurC_LIBRARIES
+    -lpthread
+)
+
+if (ENABLE_SOCKET_STREAM)
+    list(APPEND PurC_SYSTEM_INCLUDE_DIRECTORIES
+    )
+
+    list(APPEND PurC_LIBRARIES
+    )
+endif ()
+
+configure_file(ports/linux/purc.pc.in ${PurC_PKGCONFIG_FILE} @ONLY)
+install(FILES "${PurC_PKGCONFIG_FILE}"
+        DESTINATION "${LIB_INSTALL_DIR}/pkgconfig"
+)
+
