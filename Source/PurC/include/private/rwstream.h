a422ffa89a6603a759a26e1afc995cda0e0d07a9
xueshuming <xueshuming@minigui.org>
2021-07-07 11:25:42 +0800

    move rwstream error message code into rwstream.c

---
+++ Source/PurC/include/private/rwstream.h
@@ -0,0 +1,31 @@
+/*
+ * @file rwstream.h
+ * @author XueShuming
+ * @date 2021/07/07
+ * @brief The structures for PurC instance.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PURC_PRIVATE_RWSTREAM_H
+#define PURC_PRIVATE_RWSTREAM_H
+
+void pcrwstream_init(void);
+
+#endif /* not defined PURC_PRIVATE_RWSTREAM_H */
+
