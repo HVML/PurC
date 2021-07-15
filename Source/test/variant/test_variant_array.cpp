2ab7cf63f37a67507c10538336bb0348d879c807
Xu Xiaohong <xuxiaohong@minigui.net>
2021-07-15 19:09:19 +0800

    add variant_set test case

--- Source/test/variant/test_variant_array.cpp
+++ Source/test/variant/test_variant_array.cpp
@@ -634,3 +634,41 @@ TEST(variant_set, init_with_1_str)
     ASSERT_EQ (cleanup, true);
 }
 
+TEST(variant_set, init_0_elem)
+{
+    purc_instance_extra_info info = {0, 0};
+    int ret = 0;
+    bool cleanup = false;
+    struct purc_variant_stat *stat;
+
+    ret = purc_init ("cn.fmsoft.hybridos.test", "test_init", &info);
+    ASSERT_EQ(ret, PURC_ERROR_OK);
+
+    stat = purc_variant_usage_stat();
+    ASSERT_NE(stat, nullptr);
+
+    purc_variant_t var = purc_variant_make_set_c(0, "hello", NULL);
+    ASSERT_NE(var, nullptr);
+    ASSERT_EQ(stat->nr_values[PVT(_SET)], 1);
+    ASSERT_EQ(var->refc, 1);
+
+    // purc_variant_ref(var);
+    // ASSERT_EQ(stat->nr_values[PVT(_SET)], 1);
+    // ASSERT_EQ(stat->nr_values[PVT(_STRING)], 1);
+    // purc_variant_unref(var);
+
+    purc_variant_unref(var);
+    ASSERT_EQ(stat->nr_values[PVT(_SET)], 0);
+
+#if 0
+    // testing anonymous object
+    var = purc_variant_make_set(1,
+            purc_variant_make_set(1,
+                purc_variant_make_null()));
+    ASSERT_NE(var, nullptr);
+    purc_variant_unref(var);
+#endif // 0
+    cleanup = purc_cleanup ();
+    ASSERT_EQ (cleanup, true);
+}
+
