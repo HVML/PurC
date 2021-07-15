6c90b9c95859c094b23c5c32675755f0673ed764
xueshuming <xueshuming@minigui.org>
2021-07-15 15:39:33 +0800

    add test for tree

---
+++ Source/test/tree/test_tree.cpp
@@ -0,0 +1,14 @@
+#include "private/tree.h"
+
+#include <stdio.h>
+#include <errno.h>
+#include <gtest/gtest.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+TEST(tree, append)
+{
+}
+
