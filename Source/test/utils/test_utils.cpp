ae83f9d14b1b1059ef52d229b4d97513904c0233
Vincent Wei <vincent@minigui.org>
2021-07-16 09:46:05 +0800

    more cases

--- Source/test/utils/test_utils.cpp
+++ Source/test/utils/test_utils.cpp
@@ -15,6 +15,7 @@ TEST(utils, atom_basic)
     int ret = purc_init ("cn.fmsoft.hybridos.test", "variant", NULL);
     ASSERT_EQ (ret, PURC_ERROR_OK);
     char buff [] = "HVML";
+    const char *str;
 
     purc_atom_t atom;
 
@@ -26,6 +27,9 @@ TEST(utils, atom_basic)
 
     atom = purc_atom_from_static_string("HVML");
     ASSERT_EQ(atom, 1);
+
+    str = purc_atom_to_string(1);
+    ASSERT_STREQ(str, "HVML");
 
     atom = purc_atom_try_string("HVML");
     ASSERT_EQ(atom, 1);
@@ -47,6 +51,15 @@ TEST(utils, atom_basic)
 
     atom = purc_atom_try_string("PurC");
     ASSERT_EQ(atom, 2);
+
+    str = purc_atom_to_string(1);
+    ASSERT_STREQ(str, "HVML");
+
+    str = purc_atom_to_string(2);
+    ASSERT_STREQ(str, "PurC");
+
+    str = purc_atom_to_string(3);
+    ASSERT_EQ(str, nullptr);
 
     atom = purc_atom_try_string(NULL);
     ASSERT_EQ(atom, 0);
