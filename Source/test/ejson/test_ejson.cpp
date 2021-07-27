8807650550cd535603cabed7a2c756c91ef89b09
xueshuming <xueshuming@minigui.org>
2021-07-27 10:30:34 +0800

    ejson test case parse_unquoted

--- Source/test/ejson/test_ejson.cpp
+++ Source/test/ejson/test_ejson.cpp
@@ -1525,3 +1525,59 @@ TEST(ejson_token, parse_text)
     pcejson_destroy(parser);
 }
 
+TEST(ejson_token, parse_unquoted_key)
+{
+    char json[1024] = "{k_e-y9:'v'}";
+
+    purc_rwstream_t rws = purc_rwstream_new_from_mem(json, strlen(json));
+    struct pcejson* parser = pcejson_create(10, 1);
+
+    struct pcejson_token* token = pcejson_next_token(parser, rws);
+    ASSERT_NE(token, nullptr);
+    ASSERT_EQ(token->type, ejson_token_start_object);
+    ASSERT_STREQ(token->buf, nullptr);
+    pcejson_token_destroy(token);
+    pcejson_token_destroy(token);
+
+    token = pcejson_next_token(parser, rws);
+    ASSERT_NE(token, nullptr);
+    ASSERT_EQ(token->type, ejson_token_key);
+    ASSERT_STREQ(token->buf, "k_e-y9");
+    pcejson_token_destroy(token);
+    pcejson_token_destroy(token);
+
+    token = pcejson_next_token(parser, rws);
+    ASSERT_NE(token, nullptr);
+    ASSERT_EQ(token->type, ejson_token_string);
+    ASSERT_STREQ(token->buf, "v");
+    pcejson_token_destroy(token);
+
+    token = pcejson_next_token(parser, rws);
+    ASSERT_NE(token, nullptr);
+    ASSERT_EQ(token->type, ejson_token_end_object);
+    ASSERT_STREQ(token->buf, nullptr);
+    pcejson_token_destroy(token);
+
+    token = pcejson_next_token(parser, rws);
+    ASSERT_EQ(token, nullptr);
+    pcejson_token_destroy(token);
+
+    purc_rwstream_destroy(rws);
+
+    // reset
+    pcejson_reset(parser, 10, 1);
+    strcpy(json, "{1key:'v'}");
+    rws = purc_rwstream_new_from_mem(json, strlen(json));
+
+    token = pcejson_next_token(parser, rws);
+    ASSERT_NE(token, nullptr);
+    ASSERT_EQ(token->type, ejson_token_start_object);
+    ASSERT_STREQ(token->buf, nullptr);
+    pcejson_token_destroy(token);
+
+    token = pcejson_next_token(parser, rws);
+    ASSERT_EQ(token, nullptr);
+
+    purc_rwstream_destroy(rws);
+    pcejson_destroy(parser);
+}
