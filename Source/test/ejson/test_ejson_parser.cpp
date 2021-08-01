55d250aed5d1cd19e28033d96c3b0ea0f792604a
xueshuming <xueshuming@minigui.org>
2021-08-01 21:39:45 +0800

    add ejson/data for ejson test case

--- Source/test/ejson/test_ejson_parser.cpp
+++ Source/test/ejson/test_ejson_parser.cpp
@@ -4,6 +4,7 @@
 #include "purc-rwstream.h"
 
 #include <stdio.h>
+#include <unistd.h>
 #include <errno.h>
 #include <gtest/gtest.h>
 
@@ -12,22 +13,23 @@
 #include <fcntl.h>
 #include <math.h>
 
+#include <utility>
+#include <vector>
+
 using namespace std;
 
-struct T {
-    const char* json;
-    const char* comp;
-};
+typedef std::pair<std::string, std::string> ejson_test_data;
 
-class ejson_parser_vcm_eval : public testing::TestWithParam<struct T>
+class ejson_parser_vcm_eval : public testing::TestWithParam<ejson_test_data>
 {
 protected:
     void SetUp() {
-        t.json = GetParam().json;
-        t.comp = GetParam().comp;
+        json = GetParam().first;
+        comp = GetParam().second;
     }
     void TearDown() {}
-    struct T t;
+    string json;
+    string comp;
 };
 
 TEST_P(ejson_parser_vcm_eval, test0)
@@ -35,7 +37,8 @@ TEST_P(ejson_parser_vcm_eval, test0)
     int ret = purc_init ("cn.fmsoft.hybridos.test", "ejson", NULL);
     ASSERT_EQ (ret, PURC_ERROR_OK);
 
-    purc_rwstream_t rws = purc_rwstream_new_from_mem((void*)t.json, strlen(t.json));
+    purc_rwstream_t rws = purc_rwstream_new_from_mem((void*)json.c_str(),
+            json.size());
 
     struct pcvcm_node* root = NULL;
     struct pcejson* parser = NULL;
@@ -54,7 +57,7 @@ TEST_P(ejson_parser_vcm_eval, test0)
             0, PCVARIANT_SERIALIZE_OPT_PLAIN, &len_expected);
     ASSERT_GT(n, 0);
     buf[n] = 0;
-    ASSERT_STREQ(buf, t.comp);
+    ASSERT_STREQ(buf, comp.c_str());
 
     purc_variant_unref(vt);
     purc_rwstream_destroy(my_rws);
@@ -78,6 +81,88 @@ TEST_P(ejson_parser_vcm_eval, test0)
     }
 #endif
 
+char* read_file (const char* file)
+{
+    FILE* fp = fopen (file, "r");
+    if (!fp) {
+        return NULL;
+    }
+    fseek (fp, 0, SEEK_END);
+    size_t sz = ftell (fp);
+    char* buf = (char*) malloc(sz + 1);
+    fseek (fp, 0, SEEK_SET);
+    sz = fread (buf, 1, sz, fp);
+    fclose (fp);
+    return buf;
+}
+
+char* trim(char *str)
+{
+    char *end;
+
+    while (isspace((unsigned char)*str)) {
+        str++;
+    }
+
+    if(*str == 0) {
+        return str;
+    }
+
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end)) {
+        end--;
+    }
+
+    end[1] = '\0';
+    return str;
+}
+
+std::vector<ejson_test_data> read_ejson_test_data()
+{
+    std::vector<ejson_test_data> vec;
+    char* data_path = getenv("EJSON_DATA_PATH");
+
+    if (data_path) {
+        char file_path[1024] = {0};
+        strcpy (file_path, data_path);
+        strcat (file_path, "/test_list");
+
+        FILE* fp = fopen(file_path, "r");
+        if (fp) {
+            char file[1024] = {0};
+
+            char* line = NULL;
+            size_t sz = 0;
+            getline(&line, &sz, fp);
+
+            if (line) {
+                char* name = trim (line);
+                sprintf(file, "%s/%s.json", data_path, name);
+                char* json_buf = read_file (file);
+                sprintf(file, "%s/%s.serial", data_path, name);
+                char* comp_buf = read_file (file);
+
+                vec.push_back(make_pair(trim(json_buf), trim(comp_buf)));
+                free (json_buf);
+                free (comp_buf);
+                free (line);
+            }
+            fclose(fp);
+        }
+    }
+
+    if (vec.empty())
+    {
+        vec.push_back(make_pair("[123.456e-789]", "[0]"));
+    }
+    return vec;
+}
+
+
+INSTANTIATE_TEST_CASE_P(ejson, ejson_parser_vcm_eval,
+        testing::ValuesIn(read_ejson_test_data()));
+
+#if 0
 INSTANTIATE_TEST_CASE_P(ejson, ejson_parser_vcm_eval, testing::Values(
     T {
         "[123.456e-789]",
@@ -124,10 +209,5 @@ INSTANTIATE_TEST_CASE_P(ejson, ejson_parser_vcm_eval, testing::Values(
         "[\"\\uD888\\u1234\"]",
         "[\"\\\\uD888\\\\u1234\"]",
     }
-
-
-
-
-
 ));
-
+#endif
