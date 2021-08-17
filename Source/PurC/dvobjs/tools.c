466c964072cdf85e642091179f63808c4a6da6e4
gengyue <gengyue@minigui.org>
2021-08-17 21:02:06 +0800

    for home

---
+++ Source/PurC/dvobjs/tools.c
@@ -0,0 +1,104 @@
+/*
+ * @file tools.c
+ * @author Geng Yue
+ * @date 2021/07/02
+ * @brief The implementation of tools for all files in this directory.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "private/instance.h"
+#include "private/errors.h"
+#include "private/debug.h"
+#include "private/utils.h"
+#include "private/edom.h"
+#include "private/html.h"
+
+#include "purc-variant.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/utsname.h>
+#include <sys/time.h>
+
+// https://gitlab.fmsoft.cn/hvml/docs-undisclosed/blob/master/design/purc-architecture-zh.md#4102-_l
+const char* get_next_option (const char* data, const char* delims, 
+                                                            size_t* length)
+{
+    const char* head = data;
+    char* temp = NULL;
+
+    if ((delims == NULL) || (data == NULL) || (*delims == 0x00))
+        return NULL;
+
+    *length = 0;
+
+    while (*data != 0x00) {
+        temp = strchr (delims, *data);
+        if (temp) {
+            if (head == data) {
+                head = data + 1;
+            }
+            else 
+                break;
+        }
+        data++;
+    }
+
+    *length = data - head;
+    if (*length == 0)
+        head = NULL;
+
+    return head;
+}
+
+const char* get_prev_option (const char* data, size_t str_len, 
+                            const char* delims, size_t* length)
+{
+    const char* head = NULL;
+    size_t tail = *length;
+    char* temp = NULL;
+
+    if ((delims == NULL) || (data == NULL) || (*delims == 0x00) ||
+                                                        (str_len == 0))
+        return NULL;
+
+    *length = 0;
+
+    while (str_len) {
+        temp = strchr (delims, *(data + str_len - 1));
+        if (temp) {
+            if (temp == (data + str_len - 1)) {
+                str_len--;
+                tail = str_len;
+            }
+            else 
+                break;
+        }
+        str_len--;
+    }
+
+    *length = tail - str_len;
+    if (*length == 0)
+        head = NULL;
+    else
+        head = data + str_len;
+
+    return head;
+}
