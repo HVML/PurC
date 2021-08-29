959a9fe0e9610cd1f5bc190b9b47af6d78cd0008
xueshuming <xueshuming@minigui.org>
2021-08-29 16:59:11 +0800

    add hvml-token.h/c

---
+++ Source/PurC/hvml/hvml-token.h
@@ -0,0 +1,41 @@
+/*
+ * @file hvml-token.h
+ * @author XueShuming
+ * @date 2021/08/29
+ * @brief The interfaces for hvml token.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PURC_HVML_TOKEN_H
+#define PURC_HVML_TOKEN_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif /* not defined PURC_HVML_TOKEN_H */
+
