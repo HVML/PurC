8801fa5faed34a90f6c4727e88a6dc52b84a8151
xueshuming <xueshuming@minigui.org>
2021-09-08 15:57:33 +0800

    impl  eJSON keyword state

--- Source/PurC/hvml/hvml.c
+++ Source/PurC/hvml/hvml.c
@@ -2822,6 +2822,90 @@ next_state:
         END_STATE()
 
         BEGIN_STATE(PCHVML_EJSON_KEYWORD_STATE)
+            if (is_whitespace(character) || character == '}'
+                    || character == ']' || character == ',' ) {
+                RECONSUME_IN(PCHVML_EJSON_AFTER_KEYWORD_STATE);
+            }
+            if (character == '$') {
+                if (hvml->curr_vcm_node) {
+                    pcvcm_stack_push(hvml->vcm_node_stack, hvml->curr_vcm_node);
+                }
+                hvml->curr_vcm_node = pcvcm_node_new_concat_string(0, NULL);
+                pcutils_stack_push(hvml->ejson_nesting_stack, 'U');
+                if (!pchvml_temp_buffer_is_empty(hvml->temp_buffer)) {
+                    struct pcvcm_node* node = pcvcm_node_new_string(
+                            pchvml_temp_buffer_get_buffer(hvml->temp_buffer)
+                            );
+                    pctree_node_append_child(
+                            (struct pctree_node*)hvml->current_token,
+                            (struct pctree_node*)node);
+                    RESET_TEMP_BUFFER();
+                }
+                RECONSUME_IN(PCHVML_EJSON_CONTROL_STATE);
+            }
+            if (character == 't' || character == 'f' || character == 'n') {
+                if (pchvml_temp_buffer_is_empty(hvml->temp_buffer)) {
+                    APPEND_TEMP_BUFFER(c, nr_c);
+                    ADVANCE_TO(PCHVML_EJSON_KEYWORD_STATE);
+                }
+                PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_JSON_KEYWORD);
+                RETURN_AND_STOP_PARSE();
+            }
+            if (character == 'r') {
+                if (pchvml_temp_buffer_equal_to(hvml->temp_buffer, "t", 1)) {
+                    APPEND_TEMP_BUFFER(c, nr_c);
+                    ADVANCE_TO(PCHVML_EJSON_KEYWORD_STATE);
+                }
+                PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_JSON_KEYWORD);
+                RETURN_AND_STOP_PARSE();
+            }
+            if (character == 'u') {
+                if (pchvml_temp_buffer_equal_to(hvml->temp_buffer, "tr", 2)
+                   || pchvml_temp_buffer_equal_to(hvml->temp_buffer, "n", 1)) {
+                    APPEND_TEMP_BUFFER(c, nr_c);
+                    ADVANCE_TO(PCHVML_EJSON_KEYWORD_STATE);
+                }
+                PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_JSON_KEYWORD);
+                RETURN_AND_STOP_PARSE();
+            }
+            if (character == 'e') {
+                if (pchvml_temp_buffer_equal_to(hvml->temp_buffer, "tru", 3)
+                   || pchvml_temp_buffer_equal_to(hvml->temp_buffer, "fals", 4)
+                   ) {
+                    APPEND_TEMP_BUFFER(c, nr_c);
+                    ADVANCE_TO(PCHVML_EJSON_KEYWORD_STATE);
+                }
+                PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_JSON_KEYWORD);
+                RETURN_AND_STOP_PARSE();
+            }
+            if (character == 'a') {
+                if (pchvml_temp_buffer_equal_to(hvml->temp_buffer, "f", 1)) {
+                    APPEND_TEMP_BUFFER(c, nr_c);
+                    ADVANCE_TO(PCHVML_EJSON_KEYWORD_STATE);
+                }
+                PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_JSON_KEYWORD);
+                RETURN_AND_STOP_PARSE();
+            }
+            if (character == 'l') {
+                if (pchvml_temp_buffer_equal_to(hvml->temp_buffer, "nu", 2)
+                 || pchvml_temp_buffer_equal_to(hvml->temp_buffer, "nul", 3)
+                 || pchvml_temp_buffer_equal_to(hvml->temp_buffer, "fa", 2)) {
+                    APPEND_TEMP_BUFFER(c, nr_c);
+                    ADVANCE_TO(PCHVML_EJSON_KEYWORD_STATE);
+                }
+                PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_JSON_KEYWORD);
+                RETURN_AND_STOP_PARSE();
+            }
+            if (character == 's') {
+                if (pchvml_temp_buffer_equal_to(hvml->temp_buffer, "fal", 3)) {
+                    APPEND_TEMP_BUFFER(c, nr_c);
+                    ADVANCE_TO(PCHVML_EJSON_KEYWORD_STATE);
+                }
+                PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_JSON_KEYWORD);
+                RETURN_AND_STOP_PARSE();
+            }
+            PCHVML_SET_ERROR(PCHVML_ERROR_UNEXPECTED_CHARACTER);
+            RETURN_AND_STOP_PARSE();
         END_STATE()
 
         BEGIN_STATE(PCHVML_EJSON_AFTER_KEYWORD_STATE)
