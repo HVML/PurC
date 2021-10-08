1abef9efe6b532dfadd6bb505661e83248a0b373
gengyue <gengyue@minigui.org>
2021-10-08 11:37:26 +0800

    modified file name in test_files/fs

--- Source/test/dvobjs/prepare.sh
+++ Source/test/dvobjs/prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir test_files/fs
-cp "test_files/and.test" test_files/fs
-cp "test_files/and.test" test_files/fs/and.md
-cp "test_files/contains.test" test_files/fs
-cp "test_files/and.test" test_files/fs/contains.md
-cp "test_files/streq.test" test_files/fs/
+cp "test_files/and.test" test_files/fs/wildcard1.test
+cp "test_files/and.test" test_files/fs/wildcard1.md
+cp "test_files/contains.test" test_files/fs/wildcard2.test
+cp "test_files/and.test" test_files/fs/wildcard2.md
+cp "test_files/streq.test" test_files/fs/wildcard3.md
