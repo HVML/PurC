d739991d850d8689fa89f10beb29aac89c539694
Vincent Wei <vincent@minigui.org>
2021-10-19 09:01:00 +0800

    list crashed tests

--- Source/test/run_all_tests.sh
+++ Source/test/run_all_tests.sh
@@ -7,10 +7,14 @@ total_failed=0
 total_crashed=0
 
 test_failed=""
+test_crashed=""
 for x in $TEST_PROGS; do
     ./$x
     if test "$?" -eq 0; then
         total_passed=$((total_passed + 1))
+    elif test "$?" -gt 128; then
+        total_crashed=$((total_crashed + 1))
+        test_crashed="$x $test_crashed"
     else
         total_failed=$((total_failed + 1))
         test_failed="$x $test_failed"
@@ -27,8 +31,15 @@ echo "# Crashed:          $total_crashed"
 echo "#######"
 
 if test $total_failed -ne 0; then
-    echo "Crashed tests:"
+    echo "Failed tests:"
     for x in $test_failed; do
+        echo $x
+    done
+fi
+
+if test $total_crashed -ne 0; then
+    echo "Crashed tests:"
+    for x in $test_crashed; do
         echo $x
     done
 fi
