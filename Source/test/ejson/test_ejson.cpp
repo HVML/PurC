289e782e41677ccfd1d21992e1507d6eafb3a126
Xu Xiaohong <xuxiaohong@minigui.net>
2021-11-04 18:28:43 +0800

    ejson: add parse_number test case

--- Source/test/ejson/test_ejson.cpp
+++ Source/test/ejson/test_ejson.cpp
@@ -1765,7 +1765,7 @@ TEST(ejson_token, pcejson_parse)
     purc_cleanup ();
 }
 
-TEST(ejson_token, string_variang)
+TEST(ejson_token, string_variant)
 {
     int ret = purc_init ("cn.fmsoft.hybridos.test", "ejson", NULL);
     ASSERT_EQ (ret, PURC_ERROR_OK);
@@ -2162,5 +2162,42 @@ TEST(ejson_token, pcejson_parse_serial_empty_object)
     pcejson_destroy(parser);
     purc_cleanup ();
 
+}
+
+TEST(ejson_token, pcejson_parse_number)
+{
+    int ret = purc_init ("cn.fmsoft.hybridos.test", "ejson", NULL);
+    ASSERT_EQ (ret, PURC_ERROR_OK);
+
+    char json[] = "1234";
+    purc_rwstream_t rws = purc_rwstream_new_from_mem(json, strlen(json));
+
+    struct pcvcm_node* root = NULL;
+    struct pcejson* parser = NULL;
+    pcejson_parse (&root, &parser, rws, 0);
+    ASSERT_NE (root, nullptr);
+
+    purc_variant_t vt = pcvcm_eval (root, NULL);
+    ASSERT_NE(vt, PURC_VARIANT_INVALID);
+
+    char buf[1024];
+    purc_rwstream_t my_rws = purc_rwstream_new_from_mem(buf, sizeof(buf) - 1);
+    ASSERT_NE(my_rws, nullptr);
+
+    size_t len_expected = 0;
+    ssize_t n = purc_variant_serialize(vt, my_rws,
+            0, PCVARIANT_SERIALIZE_OPT_PLAIN, &len_expected);
+    ASSERT_GT(n, 0);
+    buf[n] = 0;
+    ASSERT_STREQ(buf, "1234");
+
+    purc_variant_unref(vt);
+    purc_rwstream_destroy(my_rws);
+    purc_rwstream_destroy(rws);
+
+    pcvcm_node_destroy (root);
+
+    pcejson_destroy(parser);
+    purc_cleanup ();
 }
 
