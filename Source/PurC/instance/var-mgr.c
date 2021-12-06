2a2ab3dadee4d2b1d01d71f19f736327d9ede9f1
xueshuming <xueshuming@minigui.org>
2021-12-06 17:43:47 +0800

    add var-mgr.h/c

---
+++ Source/PurC/instance/var-mgr.c
@@ -0,0 +1,82 @@
+/*
+ * @file var-mgr.c
+ * @author XueShuming
+ * @date 2021/12/06
+ * @brief The impl of PurC variable manager.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "purc.h"
+
+#include "config.h"
+
+#include "private/var-mgr.h"
+#include "private/errors.h"
+#include "private/utils.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+pcvarmgr_list_t pcvarmgr_list_create(void)
+{
+    return pcutils_map_create (copy_key_string,
+                free_key_string, NULL, NULL, comp_key_string, false);
+}
+
+void pcvarmgr_list_destroy(pcvarmgr_list_t list)
+{
+    pcutils_map_destroy(list);
+}
+
+bool pcvarmgr_list_add(pcvarmgr_list_t list, const char* name,
+        purc_variant_t variant)
+{
+    if (pcutils_map_find_replace_or_insert(list, name,
+                (void *)variant, NULL)) {
+        return false;
+    }
+
+    return true;
+}
+
+purc_variant_t pcvarmgr_list_get(pcvarmgr_list_t list, const char* name)
+{
+    const pcutils_map_entry* entry = NULL;
+
+    if (name == NULL) {
+        return PURC_VARIANT_INVALID;
+    }
+
+    if ((entry = pcutils_map_find(list, name))) {
+        return (purc_variant_t) entry->val;
+    }
+
+    return PURC_VARIANT_INVALID;
+}
+
+bool pcvarmgr_list_remove(pcvarmgr_list_t list, const char* name)
+{
+    if (name) {
+        if (pcutils_map_erase (list, (void*)name))
+            return true;
+    }
+    return false;
+}
+
+
