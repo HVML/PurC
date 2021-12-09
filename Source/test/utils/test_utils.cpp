462a9cc8e41b2ddeb844a0e36b34843922d1b030
Xu Xiaohong <xuxiaohong@minigui.net>
2021-12-09 11:46:34 +0800

    variant: adjust test cases

--- Source/test/utils/test_utils.cpp
+++ Source/test/utils/test_utils.cpp
@@ -1,4 +1,5 @@
 #include "purc.h"
+#include "private/avl.h"
 #include "private/sorted-array.h"
 
 #include <stdio.h>
@@ -180,5 +181,70 @@ TEST(utils, sorted_array_desc)
     }
 
     sorted_array_destroy(sa);
+}
+
+struct _avl_node {
+    struct avl_node          node;
+    size_t                   key;
+    size_t                   val;
+};
+static struct _avl_node* _make_avl_node(int key, int val)
+{
+    struct _avl_node *p;
+    p = (struct _avl_node*)calloc(1, sizeof(*p));
+    if (!p) return nullptr;
+    p->key = key;
+    p->val = val;
+    p->node.key = &p->key;
+    return p;
+}
+
+static int _avl_tree_comp(const void *k1, const void *k2, void *ptr)
+{
+    (void)ptr;
+    int delta = (*(size_t*)k1) - (*(size_t*)k2);
+    return delta;
+}
+
+static int _get_random(int max)
+{
+    static int seeded = 0;
+    if (!seeded) {
+        srand(time(0));
+        seeded = 1;
+    }
+
+    if (max==0)
+        return 0;
+
+    return (max<0) ? rand() : rand() % max;
+}
+
+TEST(avl, init)
+{
+    struct avl_tree avl;
+    pcutils_avl_init(&avl, _avl_tree_comp, false, NULL);
+    int r;
+    int count = 10240;
+    for (int i=0; i<count; ++i) {
+        size_t key = _get_random(-1);
+        if (pcutils_avl_find(&avl, &key)) {
+            --i;
+            continue;
+        }
+        struct _avl_node *p = _make_avl_node(key, _get_random(0));
+        r = pcutils_avl_insert(&avl, &p->node);
+        ASSERT_EQ(r, 0);
+    }
+    struct _avl_node *p, *tmp;
+    int i = 0;
+    size_t prev;
+    avl_remove_all_elements(&avl, p, node, tmp) {
+        if (i>0) {
+            ASSERT_GT(p->key, prev);
+        }
+        prev = p->key;
+        free(p);
+    }
 }
 
--- Source/test/variant/test_variant_set.cpp
+++ Source/test/variant/test_variant_set.cpp
@@ -1,5 +1,4 @@
 #include "purc.h"
-#include "private/avl.h"
 #include "private/hashtable.h"
 #include "purc-variant.h"
 #include "private/variant.h"
@@ -8,20 +7,6 @@
 #include <string.h>
 #include <errno.h>
 #include <gtest/gtest.h>
-
-static int _get_random(int max)
-{
-    static int seeded = 0;
-    if (!seeded) {
-        srand(time(0));
-        seeded = 1;
-    }
-
-    if (max==0)
-        return 0;
-
-    return (max<0) ? rand() : rand() % max;
-}
 
 static inline bool
 sanity_check(purc_variant_t set)
@@ -613,56 +598,5 @@ TEST(variant_set, sort)
 
     cleanup = purc_cleanup ();
     ASSERT_EQ (cleanup, true);
-}
-
-struct _avl_node {
-    struct avl_node          node;
-    size_t                   key;
-    size_t                   val;
-};
-static struct _avl_node* _make_avl_node(int key, int val)
-{
-    struct _avl_node *p;
-    p = (struct _avl_node*)calloc(1, sizeof(*p));
-    if (!p) return nullptr;
-    p->key = key;
-    p->val = val;
-    p->node.key = &p->key;
-    return p;
-}
-
-static int _avl_tree_comp(const void *k1, const void *k2, void *ptr)
-{
-    UNUSED_PARAM(ptr);
-    int delta = (*(size_t*)k1) - (*(size_t*)k2);
-    return delta;
-}
-
-TEST(avl, init)
-{
-    struct avl_tree avl;
-    pcutils_avl_init(&avl, _avl_tree_comp, false, NULL);
-    int r;
-    int count = 10240;
-    for (int i=0; i<count; ++i) {
-        size_t key = _get_random(-1);
-        if (pcutils_avl_find(&avl, &key)) {
-            --i;
-            continue;
-        }
-        struct _avl_node *p = _make_avl_node(key, _get_random(0));
-        r = pcutils_avl_insert(&avl, &p->node);
-        ASSERT_EQ(r, 0);
-    }
-    struct _avl_node *p, *tmp;
-    int i = 0;
-    size_t prev;
-    avl_remove_all_elements(&avl, p, node, tmp) {
-        if (i>0) {
-            ASSERT_GT(p->key, prev);
-        }
-        prev = p->key;
-        free(p);
-    }
 }
 
