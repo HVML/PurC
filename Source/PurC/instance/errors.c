c0d1b15aa689d3fb4faf72445983a09b9e6bc63e
xueshuming <xueshuming@minigui.org>
2021-12-15 21:42:37 +0800

    tune

--- Source/PurC/instance/errors.c
+++ Source/PurC/instance/errors.c
@@ -126,8 +126,7 @@ bool is_except_exinfo_requited(purc_atom_t except)
 {
     const pcutils_map_entry* entry = NULL;
     if ((entry = pcutils_map_find(purc_except_exinfo_required_map,
-                    (const void*)except)))
-    {
+                    (const void*)except))) {
         return (bool) entry->val;
     }
     return false;
