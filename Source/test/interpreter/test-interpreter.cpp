698ff02089460b12fe8d2bf9bb1f82f1c57f2538
Xu Xiaohong <xuxiaohong@minigui.net>
2021-12-15 14:40:48 +0800

    interpreter: add test case; adjust run_all_tests.sh

---
+++ Source/test/interpreter/test-interpreter.cpp
@@ -0,0 +1,27 @@
+#include "purc.h"
+
+#include <gtest/gtest.h>
+
+TEST(interpreter, basic)
+{
+    // const char *hvml = "<hvml></hvml>";
+    purc_instance_extra_info info = {0, 0};
+    int ret = 0;
+    bool cleanup = false;
+
+    // initial purc
+    ret = purc_init ("cn.fmsoft.hybridos.test", "test_init", &info);
+
+    ASSERT_EQ (ret, PURC_ERROR_OK);
+
+    // get statitics information
+    struct purc_variant_stat * stat = purc_variant_usage_stat ();
+    ASSERT_NE(stat, nullptr);
+
+    // purc_vdom_t vdom = purc_load_hvml_from_string(hvml);
+    // ASSERT_NE(vdom, nullptr);
+
+    cleanup = purc_cleanup ();
+    ASSERT_EQ (cleanup, true);
+}
+
