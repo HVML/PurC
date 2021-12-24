2067137723e5331d6c4aa6178e3bded2f3279b9b
Xu Xiaohong <xuxiaohong@minigui.net>
2021-12-24 10:46:31 +0800

    interpreter: clear error when success

--- Source/PurC/interpreter/archetype.c
+++ Source/PurC/interpreter/archetype.c
@@ -116,6 +116,7 @@ after_pushed(pcintr_coroutine_t co, struct pcintr_stack_frame *frame)
     frame->next_step = NEXT_STEP_SELECT_CHILD;
     frame->ctxt_destroy = ctxt_destroy;
     co->state = CO_STATE_READY;
+    purc_clr_error();
 }
 
 static void
@@ -133,6 +134,7 @@ on_popping(pcintr_coroutine_t co, struct pcintr_stack_frame *frame)
     }
     pcintr_pop_stack_frame(stack);
     co->state = CO_STATE_READY;
+    purc_clr_error();
 }
 
 static void
@@ -159,6 +161,7 @@ on_element(pcintr_coroutine_t co, struct pcintr_stack_frame *frame,
     ctxt->curr = &element->node;
     frame->next_step = NEXT_STEP_SELECT_CHILD;
     co->state = CO_STATE_READY;
+    purc_clr_error();
 }
 
 static void
@@ -175,6 +178,7 @@ on_content(pcintr_coroutine_t co, struct pcintr_stack_frame *frame,
     ctxt->curr = &content->node;
     frame->next_step = NEXT_STEP_SELECT_CHILD;
     co->state = CO_STATE_READY;
+    purc_clr_error();
 }
 
 static void
@@ -191,6 +195,7 @@ on_comment(pcintr_coroutine_t co, struct pcintr_stack_frame *frame,
     ctxt->curr = &comment->node;
     frame->next_step = NEXT_STEP_SELECT_CHILD;
     co->state = CO_STATE_READY;
+    purc_clr_error();
 }
 
 static void
@@ -212,6 +217,7 @@ select_child(pcintr_coroutine_t co, struct pcintr_stack_frame *frame)
     if (ctxt->curr == NULL) {
         frame->next_step = NEXT_STEP_ON_POPPING;
         co->state = CO_STATE_READY;
+        purc_clr_error();
         return;
     }
 
