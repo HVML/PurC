00b1dc0d7237e0f3d78245112416f7964eabed3d
xueshuming <xueshuming@minigui.org>
2021-12-29 17:02:26 +0800

    add  observe-handle.c

---
+++ Source/PurC/interpreter/observe-handle.c
@@ -0,0 +1,228 @@
+/**
+ * @file observe-handle.c
+ * @author Xue Shuming
+ * @date 2021/12/29
+ * @brief The ops for handle observer
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "purc.h"
+
+#include "internal.h"
+
+#include "private/debug.h"
+#include "private/runloop.h"
+
+#include "ops.h"
+
+#include <pthread.h>
+#include <unistd.h>
+#include <libgen.h>
+
+#define TO_DEBUG 0
+
+struct ctxt_for_observe_handle {
+    struct pcvdom_node           *curr;
+};
+
+static void
+ctxt_for_observe_handle_destroy(struct ctxt_for_observe_handle *ctxt)
+{
+    if (ctxt) {
+        free(ctxt);
+    }
+}
+
+static void
+ctxt_destroy(void *ctxt)
+{
+    ctxt_for_observe_handle_destroy((struct ctxt_for_observe_handle*)ctxt);
+}
+
+static void*
+after_pushed(pcintr_stack_t stack, pcvdom_element_t pos)
+{
+    PC_ASSERT(stack && pos);
+    PC_ASSERT(stack == purc_get_stack());
+
+    struct pcintr_stack_frame *frame;
+    frame = pcintr_stack_get_bottom_frame(stack);
+
+    frame->pos = pos; // ATTENTION!!
+
+    struct pcvdom_element *element = frame->pos;
+    PC_ASSERT(element);
+    D("<%s>", element->tag_name);
+
+    int r;
+    r = pcintr_element_eval_attrs(frame, element);
+    if (r)
+        return NULL;
+
+    struct ctxt_for_observe_handle *ctxt;
+    ctxt = (struct ctxt_for_observe_handle*)calloc(1, sizeof(*ctxt));
+    if (!ctxt) {
+        purc_set_error(PURC_ERROR_OUT_OF_MEMORY);
+        return NULL;
+    }
+
+    frame->ctxt = ctxt;
+    frame->ctxt_destroy = ctxt_destroy;
+
+    purc_clr_error();
+
+    return ctxt;
+}
+
+static bool
+on_popping(pcintr_stack_t stack, void* ud)
+{
+    PC_ASSERT(stack);
+    PC_ASSERT(stack == purc_get_stack());
+
+    struct pcintr_stack_frame *frame;
+    frame = pcintr_stack_get_bottom_frame(stack);
+    PC_ASSERT(frame);
+    PC_ASSERT(ud == frame->ctxt);
+
+    struct pcvdom_element *element = frame->pos;
+    PC_ASSERT(element);
+
+    struct ctxt_for_observe_handle *ctxt;
+    ctxt = (struct ctxt_for_observe_handle*)frame->ctxt;
+    if (ctxt) {
+        ctxt_for_observe_handle_destroy(ctxt);
+        frame->ctxt = NULL;
+    }
+
+    D("</%s>", element->tag_name);
+    return true;
+}
+
+static void
+on_element(pcintr_coroutine_t co, struct pcintr_stack_frame *frame,
+        struct pcvdom_element *element)
+{
+    UNUSED_PARAM(co);
+    UNUSED_PARAM(frame);
+    UNUSED_PARAM(element);
+}
+
+static void
+on_content(pcintr_coroutine_t co, struct pcintr_stack_frame *frame,
+        struct pcvdom_content *content)
+{
+    UNUSED_PARAM(co);
+    UNUSED_PARAM(frame);
+    PC_ASSERT(content);
+    char *text = content->text;
+    D("content: [%s]", text);
+}
+
+static void
+on_comment(pcintr_coroutine_t co, struct pcintr_stack_frame *frame,
+        struct pcvdom_comment *comment)
+{
+    UNUSED_PARAM(co);
+    UNUSED_PARAM(frame);
+    PC_ASSERT(comment);
+    char *text = comment->text;
+    D("comment: [%s]", text);
+}
+
+
+static pcvdom_element_t
+select_child(pcintr_stack_t stack, void* ud)
+{
+    PC_ASSERT(stack);
+    PC_ASSERT(stack == purc_get_stack());
+
+    pcintr_coroutine_t co = &stack->co;
+    struct pcintr_stack_frame *frame;
+    frame = pcintr_stack_get_bottom_frame(stack);
+    PC_ASSERT(ud == frame->ctxt);
+
+    struct ctxt_for_observe_handle *ctxt;
+    ctxt = (struct ctxt_for_observe_handle*)frame->ctxt;
+
+    struct pcvdom_node *curr;
+
+again:
+    curr = ctxt->curr;
+
+    if (curr == NULL) {
+        struct pcvdom_element *element = frame->pos;
+        struct pcvdom_node *node = &element->node;
+        node = pcvdom_node_first_child(node);
+        curr = node;
+    }
+    else {
+        curr = pcvdom_node_next_sibling(curr);
+    }
+
+    ctxt->curr = curr;
+
+    if (curr == NULL) {
+        purc_clr_error();
+        return NULL;
+    }
+
+    switch (curr->type) {
+        case PCVDOM_NODE_DOCUMENT:
+            PC_ASSERT(0); // Not implemented yet
+            break;
+        case PCVDOM_NODE_ELEMENT:
+            {
+            D("");
+                pcvdom_element_t element = PCVDOM_ELEMENT_FROM_NODE(curr);
+                on_element(co, frame, element);
+// FIXME:
+//                PC_ASSERT(stack->except == 0);
+                return element;
+            }
+        case PCVDOM_NODE_CONTENT:
+            D("");
+            on_content(co, frame, PCVDOM_CONTENT_FROM_NODE(curr));
+            goto again;
+        case PCVDOM_NODE_COMMENT:
+            D("");
+            on_comment(co, frame, PCVDOM_COMMENT_FROM_NODE(curr));
+            goto again;
+        default:
+            PC_ASSERT(0); // Not implemented yet
+    }
+
+    PC_ASSERT(0);
+    return NULL; // NOTE: never reached here!!!
+}
+
+static struct pcintr_element_ops
+ops = {
+    .after_pushed       = after_pushed,
+    .on_popping         = on_popping,
+    .rerun              = NULL,
+    .select_child       = select_child,
+};
+
+struct pcintr_element_ops* pcintr_get_observe_handle_ops(void)
+{
+    return &ops;
+}
+
