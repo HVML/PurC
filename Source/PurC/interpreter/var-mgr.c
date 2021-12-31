39153d80be7c03099b3ec1fc743e2b644de9d426
xueshuming <xueshuming@minigui.org>
2021-12-31 14:48:38 +0800

    var-mgr only set error on export api

--- Source/PurC/interpreter/var-mgr.c
+++ Source/PurC/interpreter/var-mgr.c
@@ -151,7 +151,7 @@ purc_variant_t pcvarmgr_list_get(pcvarmgr_list_t mgr, const char* name)
         return v;
     }
 
-    purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
+    //purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
     return PURC_VARIANT_INVALID;
 }
 
@@ -168,7 +168,7 @@ _find_named_scope_var(pcvdom_element_t elem, const char* name)
 {
     if (!elem || !name) {
         PC_ASSERT(name); // FIXME: still recoverable???
-        purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
+        //purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
         return PURC_VARIANT_INVALID;
     }
 
@@ -181,7 +181,7 @@ _find_named_scope_var(pcvdom_element_t elem, const char* name)
     if (parent) {
         return _find_named_scope_var(parent, name);
     }
-    purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
+    //purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
     return PURC_VARIANT_INVALID;
 }
 
@@ -190,7 +190,7 @@ _find_doc_buildin_var(purc_vdom_t vdom, const char* name)
 {
     PC_ASSERT(name);
     if (!vdom) {
-        purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
+        //purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
         return PURC_VARIANT_INVALID;
     }
 
@@ -198,7 +198,7 @@ _find_doc_buildin_var(purc_vdom_t vdom, const char* name)
     if (v) {
         return v;
     }
-    purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
+    //purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
     return PURC_VARIANT_INVALID;
 }
 
@@ -219,7 +219,7 @@ static purc_variant_t _find_inst_var(const char* name)
     if (v) {
         return v;
     }
-    purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
+    //purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
     return PURC_VARIANT_INVALID;
 }
 
@@ -249,6 +249,7 @@ pcintr_find_named_var(pcintr_stack_t stack, const char* name)
         return v;
     }
 
+    purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "name:%s", name);
     return purc_variant_make_undefined();
 }
 
@@ -297,7 +298,11 @@ pcintr_get_symbolized_var (pcintr_stack_t stack, unsigned int number,
     }
 
     purc_variant_t v = frame->symbol_vars[symbol_var];
-    return v ? v : purc_variant_make_undefined();
+    if (v != PURC_VARIANT_INVALID) {
+        return v;
+    }
+    purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "symbol:%c", symbol);
+    return purc_variant_make_undefined();
 }
 
 purc_variant_t
@@ -313,5 +318,9 @@ pcintr_get_numbered_var (pcintr_stack_t stack, unsigned int number)
     }
 
     purc_variant_t v = frame->symbol_vars[PURC_SYMBOL_VAR_QUESTION_MARK];
-    return v ? v : purc_variant_make_undefined();
+    if (v != PURC_VARIANT_INVALID) {
+        return v;
+    }
+    purc_set_error_exinfo(PCVARIANT_ERROR_NOT_FOUND, "number:%d", number);
+    return purc_variant_make_undefined();
 }
