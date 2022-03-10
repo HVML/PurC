67e7cbebd01896368086382c3efee18091da839d
xueshuming <xueshuming@minigui.org>
2022-03-10 18:29:28 +0800

    add pcintr_rdr_dom_displace_content

--- Source/PurC/interpreter/rdr.c
+++ Source/PurC/interpreter/rdr.c
@@ -34,11 +34,15 @@
 
 #include <string.h>
 
-#define ID_KEY           "id"
-#define TITLE_KEY        "title"
-#define STYLE_KEY        "style"
-#define LEVEL_KEY        "level"
-#define CLASS_KEY        "class"
+#define ID_KEY                  "id"
+#define TITLE_KEY               "title"
+#define STYLE_KEY               "style"
+#define LEVEL_KEY               "level"
+#define CLASS_KEY               "class"
+
+#define BUFF_MIN                1024
+#define BUFF_MAX                1024 * 4
+#define LEN_BUFF_LONGLONGINT    128
 
 static inline
 pcrdr_msg *make_request_msg(pcrdr_msg_target target, uint64_t target_value,
@@ -505,8 +509,6 @@ purc_attach_vdom_to_renderer(purc_vdom_t vdom,
 }
 
 
-#define BUFF_MIN            1024
-#define BUFF_MAX            1024 * 4
 bool
 pcintr_rdr_page_control_load(pcintr_stack_t stack)
 {
@@ -595,6 +597,95 @@ pcintr_rdr_page_control_load(pcintr_stack_t stack)
 failed:
     if (out) {
         purc_rwstream_destroy(out);
+    }
+
+    if (req_data != PURC_VARIANT_INVALID) {
+        purc_variant_unref(req_data);
+    }
+
+    if (msg) {
+        pcrdr_release_message(msg);
+    }
+
+    return false;
+}
+
+bool
+pcintr_rdr_dom_displace_content(pcintr_stack_t stack,
+        pcdom_node_t *node, const char *content)
+{
+    if (!pcvdom_document_is_attached_rdr(stack->vdom)) {
+        return true;
+    }
+    pcrdr_msg *msg = NULL;
+    pcrdr_msg *response_msg = NULL;
+    purc_variant_t req_data;
+    purc_variant_t element;
+    purc_vdom_t vdom = stack->vdom;
+
+    const char *operation = PCRDR_OPERATION_DISPLACE;
+    pcrdr_msg_target target = PCRDR_MSG_TARGET_DOM;
+    uint64_t target_value = pcvdom_document_get_target_dom(vdom);
+    msg = make_request_msg(target, target_value, operation);
+    if (msg == NULL) {
+        purc_set_error(PURC_ERROR_OUT_OF_MEMORY);
+        goto failed;
+    }
+
+    char buff[LEN_BUFF_LONGLONGINT];
+    int n = snprintf(buff, sizeof(buff),
+            "%llx", (unsigned long long int)(uintptr_t)node);
+    if (n < 0) {
+        purc_set_error(PURC_ERROR_BAD_STDC_CALL);
+        goto failed;
+    }
+    else if ((size_t)n >= sizeof (buff)) {
+        PC_DEBUG ("Too small buffer to serialize message.\n");
+        purc_set_error(PURC_ERROR_TOO_SMALL_BUFF);
+        goto failed;
+    }
+
+    element = purc_variant_make_string(buff, false);
+    if (element == PURC_VARIANT_INVALID) {
+        purc_set_error(PURC_ERROR_OUT_OF_MEMORY);
+        goto failed;
+    }
+
+    msg->elementType = PCRDR_MSG_ELEMENT_TYPE_HANDLE;
+    msg->element = element;
+    element = NULL;
+
+    req_data = purc_variant_make_string(content, false);
+    if (req_data == PURC_VARIANT_INVALID) {
+        purc_set_error(PURC_ERROR_OUT_OF_MEMORY);
+        goto failed;
+    }
+
+    msg->dataType = PCRDR_MSG_DATA_TYPE_TEXT;
+    msg->data = req_data;
+    req_data = NULL;
+
+    struct pcinst *inst = pcinst_current();
+    struct pcrdr_conn *conn_to_rdr = inst->conn_to_rdr;
+    if (pcrdr_send_request_and_wait_response(conn_to_rdr,
+            msg, PCRDR_TIME_DEF_EXPECTED, &response_msg) < 0) {
+        goto failed;
+    }
+    pcrdr_release_message(msg);
+    msg = NULL;
+
+    int ret_code = response_msg->retCode;
+    if (ret_code != PCRDR_SC_OK) {
+        purc_set_error(PCRDR_ERROR_SERVER_REFUSED);
+        goto failed;
+    }
+
+    pcrdr_release_message(response_msg);
+    return true;
+
+failed:
+    if (element != PURC_VARIANT_INVALID) {
+        purc_variant_unref(element);
     }
 
     if (req_data != PURC_VARIANT_INVALID) {
