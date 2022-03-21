621227535f38d9360b533550e46b1dcdd4855a6b
Vincent Wei <vincent@minigui.org>
2022-03-21 17:57:18 +0800

    remove calls to purc_log_info

--- Source/PurC/dvobjs/system.c
+++ Source/PurC/dvobjs/system.c
@@ -1353,7 +1353,6 @@ env_setter(purc_variant_t root, size_t nr_args, purc_variant_t *argv,
 
     int ret;
     if (purc_variant_is_undefined(argv[1])) {
-        purc_log_info("got undefined in %s\n", __func__);
         ret = unsetenv(name);
     }
     else {
