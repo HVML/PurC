d6a32e46515206f4d1d8da1e3bb14efd8b87018a
Xu Xiaohong <xuxiaohong@minigui.net>
2022-06-08 19:57:53 +0800

    interpreter: rbtree

--- Source/PurC/utils/rbtree.c
+++ Source/PurC/utils/rbtree.c
@@ -486,12 +486,11 @@ int pcutils_rbtree_traverse(struct rb_root *root, void *ud,
     return rbtree_traverse(node, ud, cb);
 }
 
-int pcutils_rbtree_insert(struct rb_root *root, void *ud,
+static struct rb_node**
+rbtree_find(struct rb_root *root, void *ud,
         int (*cmp)(struct rb_node *node, void *ud),
-        struct rb_node* (*new_entry)(void *ud))
+        struct rb_node **pparent)
 {
-    PC_ASSERT(root);
-
     struct rb_node **pentry;
     struct rb_node *parent;
 
@@ -508,9 +507,27 @@ int pcutils_rbtree_insert(struct rb_root *root, void *ud,
         else if (ret > 0)
             pentry = &((*pentry)->rb_right);
         else {
-            return -1;
+            break;
         }
     }
+
+    *pparent = parent;
+    return pentry;
+}
+
+int pcutils_rbtree_insert(struct rb_root *root, void *ud,
+        int (*cmp)(struct rb_node *node, void *ud),
+        struct rb_node* (*new_entry)(void *ud))
+{
+    PC_ASSERT(root);
+
+    struct rb_node **pentry;
+    struct rb_node *parent;
+
+    pentry = rbtree_find(root, ud, cmp, &parent);
+
+    if (*pentry)
+        return -1;
 
     PC_ASSERT(*pentry == NULL);
 
@@ -534,23 +551,12 @@ int pcutils_rbtree_insert_or_get(struct rb_root *root, void *ud,
     struct rb_node **pentry;
     struct rb_node *parent;
 
-    pentry = &root->rb_node;
-    parent = NULL;
-    while (*pentry) {
-        int ret;
+    pentry = rbtree_find(root, ud, cmp, &parent);
 
-        ret = cmp((*pentry), ud);
-
-        parent = *pentry;
-        if (ret < 0)
-            pentry = &((*pentry)->rb_left);
-        else if (ret > 0)
-            pentry = &((*pentry)->rb_right);
-        else {
-            if (node)
-                *node = *pentry;
-            return 0;
-        }
+    if (*pentry) {
+        if (node)
+            *node = *pentry;
+        return 0;
     }
 
     PC_ASSERT(*pentry == NULL);
@@ -568,4 +574,36 @@ int pcutils_rbtree_insert_or_get(struct rb_root *root, void *ud,
     return 0;
 }
 
+struct rb_node* pcutils_rbtree_find(struct rb_root *root, void *ud,
+        int (*cmp)(struct rb_node *node, void *ud))
+{
+    PC_ASSERT(root);
+
+    struct rb_node **pentry;
+    struct rb_node *parent;
+
+    pentry = rbtree_find(root, ud, cmp, &parent);
+
+    return *pentry;
+}
+
+int pcutils_rbtree_insert_only(struct rb_root *root, void *ud,
+        int (*cmp)(struct rb_node *node, void *ud),
+        struct rb_node *node)
+{
+    PC_ASSERT(root);
+
+    struct rb_node **pentry;
+    struct rb_node *parent;
+
+    pentry = rbtree_find(root, ud, cmp, &parent);
+
+    if (*pentry)
+        return -1;
+
+    pcutils_rbtree_link_node(node, parent, pentry);
+    pcutils_rbtree_insert_color(node, root);
+
+    return 0;
+}
 
