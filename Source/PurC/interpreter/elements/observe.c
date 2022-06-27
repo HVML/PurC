35da4e8d49a5b1e6f9988f696e3652303e2eb92e
xueshuming <xueshuming@minigui.org>
2022-06-27 15:18:24 +0800

    observe bind name by at prop

--- Source/PurC/interpreter/elements/observe.c
+++ Source/PurC/interpreter/elements/observe.c
@@ -615,6 +615,150 @@ process_variant_observer(pcintr_stack_t stack,
     return NULL;
 }
 
+static bool
+match_id(pcintr_stack_t stack, struct pcvdom_element *elem, const char *id)
+{
+    struct pcvdom_attr *attr = pcvdom_element_find_attr(elem, "id");
+    if (!attr) {
+        return false;
+    }
+
+    bool silently = false;
+    purc_variant_t v = pcvcm_eval(attr->val, stack, silently);
+    purc_clr_error();
+    if (v == PURC_VARIANT_INVALID) {
+        return false;
+    }
+
+    bool matched = false;
+
+    do {
+        if (!purc_variant_is_string(v)) {
+            break;
+        }
+        const char *sv = purc_variant_get_string_const(v);
+        if (!sv) {
+            break;
+        }
+
+        if (strcmp(sv, id) == 0) {
+            matched = true;
+        }
+    } while (0);
+
+    purc_variant_unref(v);
+    return matched;
+}
+
+static int
+bind_at_element(struct pcvdom_element *elem, const char *name,
+        purc_variant_t val)
+{
+    return pcintr_bind_scope_variable(elem, name, val) ? 0 : -1 ;
+}
+
+static int
+bind_at_vdom(purc_vdom_t vdom, const char *name, purc_variant_t val)
+{
+    return purc_bind_document_variable(vdom, name, val) ? 0 : -1;
+}
+
+static int
+bind_by_level(pcintr_stack_t stack, struct pcintr_stack_frame *frame,
+        const char *name, purc_variant_t val, uint64_t level)
+{
+    bool silently = frame->silently;
+    struct pcvdom_element *p = frame->pos;
+
+    for (uint64_t i = 0; i < level; ++i) {
+        if (p == NULL) {
+            break;
+        }
+        p = pcvdom_element_parent(p);
+    }
+
+    if (p) {
+        return bind_at_element(p, name, val);
+    }
+
+    if (silently) {
+        return bind_at_vdom(stack->vdom, name, val);
+    }
+    purc_set_error_with_info(PURC_ERROR_ENTITY_NOT_FOUND,
+            "no vdom element exists");
+    return -1;
+}
+
+static int
+process_bind_by_elem_id(pcintr_stack_t stack, struct pcintr_stack_frame *frame,
+        const char *id, const char *name, purc_variant_t val)
+{
+    struct pcvdom_element *p = frame->pos;
+    struct pcvdom_element *dest = NULL;
+    while (p) {
+        if (match_id(stack, p, id)) {
+            dest = p;
+            break;
+        }
+        p = pcvdom_element_parent(p);
+    }
+
+    if (dest) {
+        return bind_at_element(dest, name, val);
+    }
+
+    if (frame->silently) {
+        return bind_at_vdom(stack->vdom, name, val);
+    }
+
+    purc_set_error_with_info(PURC_ERROR_ENTITY_NOT_FOUND,
+            "no vdom element exists");
+    return -1;
+}
+
+static int
+process_bind_by_name_space(pcintr_stack_t stack,
+        struct pcintr_stack_frame *frame, const char *ns, const char *name,
+        purc_variant_t val)
+{
+    purc_atom_t atom = PCHVML_KEYWORD_ATOM(HVML, ns);
+    if (atom == 0) {
+        goto not_found;
+    }
+
+    if (pchvml_keyword(PCHVML_KEYWORD_ENUM(HVML, _PARENT)) == atom) {
+        return bind_by_level(stack, frame, name, val, 1);
+    }
+
+    if (pchvml_keyword(PCHVML_KEYWORD_ENUM(HVML, _GRANDPARENT)) == atom ) {
+        return bind_by_level(stack, frame, name, val, 2);
+    }
+
+    if (pchvml_keyword(PCHVML_KEYWORD_ENUM(HVML, _ROOT)) == atom ) {
+        return bind_at_vdom(stack->vdom, name, val);
+    }
+
+    if (pchvml_keyword(PCHVML_KEYWORD_ENUM(HVML, _LAST)) == atom) {
+        return bind_by_level(stack, frame, name, val, 1);
+    }
+
+    if (pchvml_keyword(PCHVML_KEYWORD_ENUM(HVML, _NEXTTOLAST)) == atom) {
+        return bind_by_level(stack, frame, name, val, 2);
+    }
+
+    if (pchvml_keyword(PCHVML_KEYWORD_ENUM(HVML, _TOPMOST)) == atom) {
+        return bind_at_vdom(stack->vdom, name, val);
+    }
+
+not_found:
+    if (frame->silently) {
+        return bind_at_vdom(stack->vdom, name, val);
+    }
+    purc_set_error_with_info(PURC_ERROR_BAD_NAME,
+            "at = '%s'", name);
+    return -1;
+}
+
 static void*
 after_pushed(pcintr_stack_t stack, pcvdom_element_t pos)
 {
@@ -709,7 +853,24 @@ after_pushed(pcintr_stack_t stack, pcvdom_element_t pos)
             pcintr_revoke_observer(observer);
             return ctxt;
         }
-        if(!pcintr_bind_document_variable(stack->vdom, name, v)) {
+
+        int bind_ret = -1;
+        if (ctxt->at && purc_variant_is_string(ctxt->at)) {
+            const char *s_at = purc_variant_get_string_const(ctxt->at);
+            if (s_at[0] == '#') {
+                bind_ret = process_bind_by_elem_id(stack, frame, s_at,
+                        name, v);
+            }
+            else {
+                bind_ret = process_bind_by_name_space(stack, frame, s_at,
+                        name, v);
+            }
+        }
+        else {
+            bind_ret = bind_at_vdom(stack->vdom, name, v);
+        }
+
+        if (bind_ret != 0) {
             purc_variant_unref(v); // on_release
             return ctxt;
         }
