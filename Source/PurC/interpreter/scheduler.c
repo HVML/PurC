37ba3e417462a14621290170b80107f06480a05c
xueshuming <xueshuming@minigui.org>
2022-07-11 17:16:58 +0800

    add scheduler.c for schedule coroutine

---
+++ Source/PurC/interpreter/scheduler.c
@@ -0,0 +1,39 @@
+/*
+ * @file scheduler.c
+ * @author XueShuming
+ * @date 2022/07/11
+ * @brief The scheduler for coroutine.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "purc.h"
+
+#include "config.h"
+
+#include "internal.h"
+
+#include "private/var-mgr.h"
+#include "private/errors.h"
+#include "private/instance.h"
+#include "private/utils.h"
+#include "private/variant.h"
+
+#include <stdlib.h>
+#include <string.h>
+
