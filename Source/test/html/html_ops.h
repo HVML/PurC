3293e80de9ce9239bc20cd3e6cfd962a26f38023
Vincent Wei <vincent@minigui.org>
2022-07-14 16:35:13 +0800

    add copying statements for source files in Source/test

--- Source/test/html/html_ops.h
+++ Source/test/html/html_ops.h
@@ -1,27 +1,22 @@
-/**
- * @file html_ops.h
- * @author Xu Xiaohong
- * @date 2021/11/18
- * @brief The internal interfaces for utilities to operate HTML document.
- *
- * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
- *
- * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- */
+/*
+** Copyright (C) 2022 FMSoft <https://www.fmsoft.cn>
+**
+** This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 #ifndef PURC_TEST_HTML_OPS_H
 #define PURC_TEST_HTML_OPS_H
--- Source/test/instance/test_threads.cpp
+++ Source/test/instance/test_threads.cpp
@@ -1,33 +1,22 @@
 /*
- * @file test-threads.c
- * @author Vincent Wei
- * @date 2022/07/07
- * @brief The program to test multiple runners; the following APIs covered:
- *      - purc_inst_create_move_buffer
- *      - purc_inst_destroy_move_buffer
- *      - purc_inst_holding_messages_count
- *      - purc_inst_take_away_message
- *      - purc_inst_move_message
- *      - pcrdr_make_event_message
- *      - pcrdr_release_message
- *
- * Copyright (C) 2022 FMSoft <https://www.fmsoft.cn>
- *
- * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+** Copyright (C) 2022 FMSoft <https://www.fmsoft.cn>
+**
+** This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 #include "purc.h"
 
