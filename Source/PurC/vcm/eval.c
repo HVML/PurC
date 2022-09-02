d53396c6ca62cb55a45db4f1839fd5ed5f6c38c4
xueshuming <xueshuming@minigui.org>
2022-09-02 10:49:02 +0800

    add eval.h/c for vcm evaluate

---
+++ Source/PurC/vcm/eval.c
@@ -0,0 +1,41 @@
+/*
+ * @file eval.c
+ * @author XueShuming
+ * @date 2021/09/02
+ * @brief The impl of vcm evaluate.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#include "config.h"
+#include "purc-utils.h"
+#include "purc-errors.h"
+#include "purc-rwstream.h"
+
+#include "private/errors.h"
+#include "private/stack.h"
+#include "private/interpreter.h"
+#include "private/utils.h"
+
+#include "eval.h"
+
