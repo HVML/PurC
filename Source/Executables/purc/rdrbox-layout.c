4f1eb8e1519a7a2cc3b71843525fc30e1822e50e
Vincent Wei <vincent@minigui.org>
2022-11-24 11:21:32 +0800

    fix a conditional compilation warning

--- Source/Executables/purc/rdrbox-layout.c
+++ Source/Executables/purc/rdrbox-layout.c
@@ -572,6 +572,7 @@ adjust_position_vertically(foil_layout_ctxt *ctxt, foil_rdrbox *box)
     (void)box;
 }
 
+#ifndef NDEBUG
 static const char *literal_values_text_align[] = {
     "left",
     "right",
@@ -583,6 +584,7 @@ static const char *literal_values_text_overflow[] = {
     "clip",
     "ellipsis",
 };
+#endif
 
 void foil_rdrbox_determine_geometry(foil_layout_ctxt *ctxt, foil_rdrbox *box)
 {
