ec590ae68f55a327c9e891becffc4b67e81a0738
Vincent Wei <vincent@minigui.org>
2023-02-19 12:34:41 +0800

    tune progress marks

--- Source/Executables/purc/rdrbox-progress.c
+++ Source/Executables/purc/rdrbox-progress.c
@@ -39,7 +39,7 @@
 #define INDICATOR_STEPS 10
 
 static const char *def_bar_marks = "━━";
-static const char *def_mark_marks = "⠁⠉⠙⠹⢹⣹⣽⣿⣾⣶⣦⣆⡆⠆⠂";
+static const char *def_mark_marks = "⣿⣾⣶⣦⣆⡆⠆⠂⢁⠁⠉⠙⠹⢹⣹⣽⣿";
 // static const char *def_mark_marks = "⠁⠈⠐⠠⢀⡀⠄⠂";
 
 struct _tailor_data {
