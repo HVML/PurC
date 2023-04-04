e7ab06dcfa97951952a03b77b364c6712b69441d
xueshuming <xueshuming@minigui.org>
2023-04-04 09:10:02 +0800

    tune

--- Source/PurC/ejson/tkz-helper.c
+++ Source/PurC/ejson/tkz-helper.c
@@ -626,14 +626,14 @@ tkz_set_error_info(struct tkz_reader *reader, struct tkz_uc *uc, int error,
     info->error = error;
 
     if (extra) {
-        purc_set_error_with_info(error, "%s:%d:%d;%s",
-                type, info->line, info->column,
-                extra);
+        purc_set_error_with_info(error, "E%d:%s:%d:%d:%s:%s",
+                error, type, info->line, info->column,
+                purc_get_error_message(error), extra);
     }
     else {
-        purc_set_error_with_info(error, "%s:%d:%d;character=0x%x",
-                type, info->line, info->column,
-                info->character);
+        purc_set_error_with_info(error, "E%d:%s:%d:%d:%s",
+                error, type, info->line, info->column,
+                purc_get_error_message(error));
     }
 
     purc_set_local_data(PURC_LDNAME_PARSE_ERROR, (uintptr_t)info,
