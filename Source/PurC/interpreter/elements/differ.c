499f5244a574a3972873e72ef48030ac7756a2f0
xueshuming <xueshuming@minigui.org>
2023-07-05 17:23:39 +0800

    differ tag : handle attr 'on'

--- Source/PurC/interpreter/elements/differ.c
+++ Source/PurC/interpreter/elements/differ.c
@@ -37,12 +37,14 @@
 
 struct ctxt_for_differ {
     struct pcvdom_node           *curr;
+    purc_variant_t                on;
 };
 
 static void
 ctxt_for_differ_destroy(struct ctxt_for_differ *ctxt)
 {
     if (ctxt) {
+        PURC_VARIANT_SAFE_CLEAR(ctxt->on);
         free(ctxt);
     }
 }
@@ -51,6 +53,49 @@ static void
 ctxt_destroy(void *ctxt)
 {
     ctxt_for_differ_destroy((struct ctxt_for_differ*)ctxt);
+}
+
+static int
+process_attr_on(struct pcintr_stack_frame *frame,
+        struct pcvdom_element *element,
+        purc_atom_t name, purc_variant_t val)
+{
+    struct ctxt_for_differ *ctxt;
+    ctxt = (struct ctxt_for_differ*)frame->ctxt;
+    if (ctxt->on != PURC_VARIANT_INVALID) {
+        purc_set_error_with_info(PURC_ERROR_DUPLICATED,
+                "vdom attribute '%s' for element <%s>",
+                purc_atom_to_string(name), element->tag_name);
+        return -1;
+    }
+    if (val == PURC_VARIANT_INVALID) {
+        purc_set_error_with_info(PURC_ERROR_INVALID_VALUE,
+                "vdom attribute '%s' for element <%s> undefined",
+                purc_atom_to_string(name), element->tag_name);
+        return -1;
+    }
+    ctxt->on = val;
+    purc_variant_ref(val);
+
+    return 0;
+}
+
+static int
+attr_found_val(struct pcintr_stack_frame *frame,
+        struct pcvdom_element *element,
+        purc_atom_t name, purc_variant_t val,
+        struct pcvdom_attr *attr,
+        void *ud)
+{
+    UNUSED_PARAM(attr);
+    UNUSED_PARAM(ud);
+
+    if (pchvml_keyword(PCHVML_KEYWORD_ENUM(HVML, ON)) == name) {
+        return process_attr_on(frame, element, name, val);
+    }
+
+    /* ignore other attr */
+    return 0;
 }
 
 static void*
@@ -73,10 +118,6 @@ after_pushed(pcintr_stack_t stack, pcvdom_element_t pos)
         }
     }
 
-    if (frame->eval_step == STACK_FRAME_EVAL_STEP_ATTR) {
-        frame->eval_step = STACK_FRAME_EVAL_STEP_CONTENT;
-    }
-
     struct ctxt_for_differ *ctxt = frame->ctxt;
     if (!ctxt) {
         ctxt = (struct ctxt_for_differ*)calloc(1, sizeof(*ctxt));
@@ -92,6 +133,22 @@ after_pushed(pcintr_stack_t stack, pcvdom_element_t pos)
 
     if (0 != pcintr_stack_frame_eval_attr_and_content(stack, frame, false)) {
         return NULL;
+    }
+
+    frame->attr_vars = purc_variant_make_object(0,
+            PURC_VARIANT_INVALID, PURC_VARIANT_INVALID);
+    if (frame->attr_vars == PURC_VARIANT_INVALID)
+        return ctxt;
+
+    struct pcvdom_element *element = frame->pos;
+
+    int r;
+    r = pcintr_walk_attrs(frame, element, stack, attr_found_val);
+    if (r)
+        return ctxt;
+
+    if (ctxt->on) {
+        pcintr_set_question_var(frame, ctxt->on);
     }
 
     if (!parent || !parent->pos || parent->pos->tag_id != PCHVML_TAG_TEST) {
