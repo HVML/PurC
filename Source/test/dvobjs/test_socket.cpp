cfd49f157c8d4e87393c84fcf81685fdef92da61
Vincent Wei <vincent@minigui.org>
2025-02-12 12:32:55 +0800

    new test for dgramSocket

--- Source/test/dvobjs/test_socket.cpp
+++ Source/test/dvobjs/test_socket.cpp
@@ -141,3 +141,26 @@ TEST(socket, local_dgram)
     }
 }
 
+TEST(socket, local_dgram_multiple)
+{
+    PurCInstance purc(false);
+
+    purc_enable_log_ex(PURC_LOG_MASK_ALL, PURC_LOG_FACILITY_STDERR);
+
+    purc_atom_t client_inst = purc_inst_create_or_get(APP_NAME,
+            "client", client_cond_handler, NULL);
+    assert(client_inst != 0);
+
+    run_one_comp_test("dvobjs/socket/local-dgram-multiple.hvml");
+
+    purc_inst_ask_to_shutdown(client_inst);
+
+    unsigned int seconds = 0;
+    while (purc_atom_to_string(client_inst)) {
+        purc_log_info("Wait for termination of client instance...\n");
+        sleep(1);
+        seconds++;
+        ASSERT_LT(seconds, 10);
+    }
+}
+
